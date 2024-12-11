32fdaef8f28ba9470afcd8b170384f1ed0701e98
aayushkananu <aayushka.budhathoki@bison.howard.edu>
2024-12-10 21:58:11 -0500

    new commit

--- apps/server/setupListeners.ts
+++ apps/server/setupListeners.ts
@@ -4,25 +4,30 @@ import { Game } from "./classes/game";
 export const rooms = new Map<string, Game>();
 
 export function setupListeners(io: Server) {
-    io.on("connection", (socket) => {
-        console.log(`New connection - ${socket.id}`);
+  io.on("connection", (socket) => {
+    console.log(`New connection - ${socket.id}`);
 
-        socket.on("join-game", (roomID: string, name: string) => {
-            if(!roomID) return socket.emit("error", "Invalid room ID");
-            if (!name) return socket.emit('error', "Please provide nickname.");
-            
-            socket.join(roomID);
+    socket.on("join-game", (roomId: string, name: string) => {
+      if (!roomId) {
+        return socket.emit("error", "Invalid room ID");
+      }
+      socket.join(roomId);
+      console.log("joined a room");
 
-            if(rooms.has(roomID)) {
-                const game = rooms.get(roomID)
-                if (!game) return socket.emit('error', 'Game not found');
-                game.joinPlayer(socket.id, name, socket);
-            } else {
-                const game = new Game(roomID, io, socket.id)
-                rooms.set(roomID, game)
-                game.joinPlayer(socket.id, name, socket);
-            }
-            
-        });
+      // Check if the game already exists
+      if (rooms.has(roomId)) {
+        console.log("room exists");
+        // Add the player to the class instance
+        const game = rooms.get(roomId);
+        if (!game) return socket.emit("error", "Game not found");
+        game.joinPlayer(socket.id, name, socket);
+      } else {
+        console.log("room doesnt exists");
+        // Create a new game instance
+        const game = new Game(roomId, io, socket.id);
+        rooms.set(roomId, game);
+        game.joinPlayer(socket.id, name, socket);
+      }
     });
+  });
 }
